2a8e9dbbcf28e6ac4b0d0fdd7ba9163e83bafb22
Roope <kolehmainen.roope@gmail.com>
2025-06-04 17:13:32 +0300

    New walk logic

--- summer-project/Scriptit/player.gd
+++ summer-project/Scriptit/player.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 class_name Player
 
-@export_category("Player Settings")
-@export var speed = 600
-@export var TILE_SIZE_pixels = 16
 
-var player_direction = Vector2.ZERO
+
+@export_category("Player Settings")
+@export var speed = 4.0
+@export var TILE_SIZE = 16
+
+var initial_position = Vector2(0,0)
+var player_direction = Vector2(0,0)
 var is_moving = false
 var percent_to_next_tile = 0.0
 
@@ -14,12 +17,35 @@ var percent_to_next_tile = 0.0
 
 
 
-func _physics_process(delta: float):
-	player_movement(delta)
-	move_and_slide()
+func _physics_process(delta):
+	if is_moving == false:
+		process_player_input()
+	elif  player_direction != Vector2.ZERO:
+		move(delta)
+	else:
+		is_moving = false
 
+
+func process_player_input():
+	if player_direction.y == 0:
+		player_direction.x = int(Input.get_action_strength("right")) - int(Input.get_action_strength("left"))
+		
+	
+	if player_direction.x == 0:
+		player_direction.y = int(Input.get_action_strength("down")) - int(Input.get_action_strength("up"))
 	
 	
-func player_movement(_delta):
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	if player_direction != Vector2.ZERO:
+		initial_position = position
+		is_moving = true
+
+
+
+func move(delta):
+	percent_to_next_tile += speed * delta
+	if percent_to_next_tile >= 0.99:
+		position = initial_position + (TILE_SIZE*player_direction)
+		percent_to_next_tile = 0.0
+		is_moving = false
+	else:
+		position = initial_position + (TILE_SIZE * player_direction * percent_to_next_tile)

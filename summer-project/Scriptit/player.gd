77003b2b2628c4c491743dcadc6d802ed6fd785a
Dashambler <kolehmainen.roope@gmail.com>
2025-06-04 15:06:44 +0300

    Update player.gd

--- summer-project/Scriptit/player.gd
+++ summer-project/Scriptit/player.gd
@@ -2,7 +2,16 @@ extends CharacterBody2D
 
 class_name Player
 
+@export_category("Player Settings")
 @export var speed = 600
+@export var TILE_SIZE_pixels = 16
+
+var player_direction = Vector2.ZERO
+var is_moving = false
+var percent_to_next_tile = 0.0
+
+
+
 
 
 func _physics_process(delta: float):

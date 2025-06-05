bcc3c1ef0da0502516917986f935923a0abd0f74
Roope <kolehmainen.roope@gmail.com>
2025-06-05 22:51:11 +0300

    Updated

--- summer-project/Scriptit/player.gd
+++ summer-project/Scriptit/player.gd
@@ -28,73 +28,72 @@ func _ready():
 func _physics_process(delta):
 	if player_state == PlayerState.TURNING:
 		return
+
 	elif is_moving == false:
 		process_player_input()
-		print(is_moving)
+
 	elif  player_direction != Vector2.ZERO:
 		move(delta)
 		anim_state.travel("Walk")
+
 	else:
 		anim_state.travel("Idle")
 		is_moving = false
-		
-
-			
-		
 
 func process_player_input():
 	if player_direction.y == 0:
 		player_direction.x = int(Input.get_action_strength("right")) - int(Input.get_action_strength("left"))
-		
-		
-	
+
 	if player_direction.x == 0:
 		player_direction.y = int(Input.get_action_strength("down")) - int(Input.get_action_strength("up"))
-
 
 	if player_direction != Vector2.ZERO:
 		anim_tree.set("parameters/Idle/blend_position", player_direction)
 		anim_tree.set("parameters/Walk/blend_position",player_direction)
 		anim_tree.set("parameters/Turn/blend_position",player_direction)
-		
+
 		if need_to_turn():
 			player_state = PlayerState.TURNING
 			anim_state.travel("Turn")
+
 		else:
 			initial_position = position
 			is_moving = true
+
 	else:
 		anim_state.travel("Idle")
 
-
-
 func move(delta):
 	percent_to_next_tile += speed * delta
+
 	if percent_to_next_tile >= 0.99:
 		position = initial_position + (TILE_SIZE*player_direction)
 		percent_to_next_tile = 0.0
 		is_moving = false
+
 	else:
 		position = initial_position + (TILE_SIZE * player_direction * percent_to_next_tile)
 
 func need_to_turn():
 	var new_facing_direction = facing_direction
+
 	if player_direction.x < 0:
 		new_facing_direction = FacingDirection.LEFT
+
 	elif player_direction.x > 0:
 		new_facing_direction = FacingDirection.RIGHT
+
 	elif player_direction.y < 0:
 		new_facing_direction = FacingDirection.UP
+
 	elif player_direction.y > 0:
 		new_facing_direction = FacingDirection.DOWN
 	
 	if facing_direction != new_facing_direction:
 		facing_direction = new_facing_direction
 		return true
-	
+
 	return false
-	
-	
+
 func finished_turning():
 	player_state = PlayerState.IDLE
-	

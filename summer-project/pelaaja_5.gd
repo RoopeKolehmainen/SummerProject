5da699612330fc41ecda8c448dcf98aaf06ffc2a
Dashambler <kolehmainen.roope@gmail.com>
2025-06-10 16:26:47 +0300

    doesnt work

--- summer-project/pelaaja_5.gd
+++ summer-project/pelaaja_5.gd
@@ -1,4 +1,3 @@
-
 extends CharacterBody2D
 
 @export_category("Settings")
@@ -6,17 +5,19 @@ var TILE_SIZE = 16
 @export var speed = 4
 @export var sprint_speed = 10
 
-enum State { IDLE, WALK, RUN }
-enum Direction { UP, LEFT, RIGHT, DOWN }
+enum State {WALK, RUN, IDLE}
+enum Direction {UP, LEFT, RIGHT, DOWN}
 
 var player_state = State.IDLE
 var facing_direction = Direction.DOWN
 
-var initial_position = Vector2.ZERO
+var initial_position = Vector2()
 var player_direction = Vector2.ZERO
 var is_moving = false
+
 var percent_to_next_tile = 0.0
-var direction_keys: Array = []
+var input_buffer = []
+var input_buffer_readout = Vector2.ZERO
 
 @onready var anim_tree = $AnimationTree
 @onready var anim_state = anim_tree.get("parameters/playback")
@@ -27,8 +28,6 @@ func _ready():
 	anim_tree.set("parameters/Idle/blend_position", Vector2.DOWN)
 
 func _physics_process(delta):
-	process_player_input()
-	
 	match player_state:
 		State.WALK:
 			walk(delta)
@@ -36,105 +35,108 @@ func _physics_process(delta):
 			run(delta)
 		State.IDLE:
 			idle()
-
+	
+	if is_moving == false:
+		process_player_input()
+	
+	# You can adjust speed here if needed
+	speed = 8
 	player_animations()
-	direction_strength()
-
-func process_player_input():
-	if is_moving:
-		return  # Don't process new input while moving
-
-	var direction_map = {
-		"left": Vector2(-1, 0),
-		"right": Vector2(1, 0),
-		"up": Vector2(0, -1),
-		"down": Vector2(0, 1)
-	}
-
-	if direction_keys.size() > 0:
-		var key = direction_keys.back()
-		player_direction = direction_map.get(key, Vector2.ZERO)
-	else:
-		player_direction = Vector2.ZERO
-		player_state = State.IDLE
-		return
-
-	if player_direction != Vector2.ZERO:
-		initial_position = position
-		is_moving = true
-		percent_to_next_tile = 0.0
-
-		# Update animation directions
-		anim_tree.set("parameters/Idle/blend_position", player_direction)
-		anim_tree.set("parameters/Walk/blend_position", player_direction)
-		anim_tree.set("parameters/Run/blend_position", player_direction)
-
-		# Choose state
-		if Input.is_action_pressed("sprint"):
-			player_state = State.RUN
-		else:
-			player_state = State.WALK
+	print(is_moving)
 
 func walk(delta):
-	var desired_step = player_direction * TILE_SIZE / 2
+	var desired_step: Vector2 = player_direction * TILE_SIZE / 2
 	ray.target_position = desired_step
 	ray.force_raycast_update()
-
-	if !ray.is_colliding():
+	if not ray.is_colliding():
 		percent_to_next_tile += speed * delta
-
 		if percent_to_next_tile >= 1.0:
-			position = initial_position + TILE_SIZE * player_direction
+			position = initial_position + (TILE_SIZE * player_direction)
 			percent_to_next_tile = 0.0
 			is_moving = false
-			player_state = State.IDLE
 		else:
-			position = initial_position + TILE_SIZE * player_direction * percent_to_next_tile
+			position = initial_position + (TILE_SIZE * player_direction * percent_to_next_tile)
 	else:
 		is_moving = false
 		percent_to_next_tile = 0.0
-		player_state = State.IDLE
 
 func run(delta):
-	var desired_step = player_direction * TILE_SIZE / 2
+	var desired_step: Vector2 = player_direction * TILE_SIZE / 2
 	ray.target_position = desired_step
 	ray.force_raycast_update()
-
-	if !ray.is_colliding():
+	if not ray.is_colliding():
 		percent_to_next_tile += sprint_speed * delta
-
 		if percent_to_next_tile >= 1.0:
-			position = initial_position + TILE_SIZE * player_direction
+			position = initial_position + (TILE_SIZE * player_direction)
 			percent_to_next_tile = 0.0
 			is_moving = false
 			player_state = State.IDLE
 		else:
-			position = initial_position + TILE_SIZE * player_direction * percent_to_next_tile
+			position = initial_position + (TILE_SIZE * player_direction * percent_to_next_tile)
 	else:
 		is_moving = false
 		percent_to_next_tile = 0.0
-		player_state = State.IDLE
 
 func idle():
-	# Nothing happens in idle except waiting for input
-	pass
+	if player_direction != Vector2.ZERO:
+		player_state = State.WALK
 
 func player_animations():
 	match player_state:
 		State.WALK:
 			anim_state.travel("Walk")
 		State.RUN:
-			anim_state.travel("Run")  # Using Run animation for run state
+			anim_state.travel("Walk")
 		State.IDLE:
 			anim_state.travel("Idle")
 
-func direction_strength():
-	var directions = ["right", "left", "up", "down"]
-	for dir in directions:
-		if Input.is_action_just_pressed(dir):
-			direction_keys.erase(dir)
-			direction_keys.push_back(dir)
-		elif Input.is_action_just_released(dir):
-			direction_keys.erase(dir)
-	if direction_keys.size() == 0:
-		direction_keys.clear()
+func process_player_input():
+	# Clear previous buffer if no new input
+	# Buffer only stores recent key presses
+	# Check for new key presses
+	if Input.is_action_just_pressed("right"):
+		input_buffer.append(Vector2.RIGHT)
+	elif Input.is_action_just_pressed("left"):
+		input_buffer.append(Vector2.LEFT)
+	elif Input.is_action_just_pressed("up"):
+		input_buffer.append(Vector2.UP)
+	elif Input.is_action_just_pressed("down"):
+		input_buffer.append(Vector2.DOWN)
+
+	# Remove keys when released
+	if Input.is_action_just_released("right"):
+		input_buffer.erase(Vector2.RIGHT)
+	elif Input.is_action_just_released("left"):
+		input_buffer.erase(Vector2.LEFT)
+	elif Input.is_action_just_released("up"):
+		input_buffer.erase(Vector2.UP)
+	elif Input.is_action_just_released("down"):
+		input_buffer.erase(Vector2.DOWN)
+
+	# Determine the last input from buffer
+	if input_buffer.size() > 0:
+		input_buffer_readout = input_buffer[-1]
+	else:
+		input_buffer_readout = Vector2.ZERO
+
+	# If there's buffered input, set player_direction accordingly
+	if input_buffer_readout != Vector2.ZERO:
+		# Only change direction if not currently moving
+		if not is_moving:
+			player_direction = input_buffer_readout
+			initial_position = position
+			is_moving = true
+			# Set animations parameters based on direction
+			anim_tree.set("parameters/Idle/blend_position", player_direction)
+			anim_tree.set("parameters/Walk/blend_position", player_direction)
+			anim_tree.set("parameters/Run/blend_position", player_direction)
+	else:
+		# No input, set to idle if not moving
+		if not is_moving:
+			player_state = State.IDLE
+
+	# Handle sprint input
+	if Input.is_action_pressed("sprint") and player_direction != Vector2.ZERO:
+		player_state = State.RUN
+	else:
+		player_state = State.WALK

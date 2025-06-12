19e9ee6ede5e7f9e73d1e6b485af36be4b1eaa43
Dashambler <kolehmainen.roope@gmail.com>
2025-06-12 16:17:50 +0300

    Flower

--- summer-project/Scriptit/grass.gd
+++ summer-project/Scriptit/grass.gd
@@ -9,5 +9,5 @@ func _ready():
 	#get_tree().current_scene.find_children()
 	pass
 	
-func _on_area_2d_body_entered(body: Node2D):
+func _on_area_2d_body_entered(_body: Node2D):
 	anim_player.play("Stepped")

00bda46699ececef8cd6c96d4eaafe0879cfe6eb
Dashambler <kolehmainen.roope@gmail.com>
2025-06-27 16:09:59 +0300

    Commented away some useless code

--- summer-project/Scriptit/pelaaja_4.gd
+++ summer-project/Scriptit/pelaaja_4.gd
@@ -137,26 +137,26 @@ func player_animations():
 		State.IDLE:
 			anim_state.travel("Idle")
 
-func movement_inputs():
+#func movement_inputs():
 		# Store direction keys in a "stack", ordered by when they're pressed
-	if Input.is_action_just_pressed("right"):
-		direction_keys.push_back("right")
-	elif Input.is_action_just_released("right"):
-		direction_keys.erase("right")
-	if Input.is_action_just_pressed("left"):
-		direction_keys.push_back("left")
-	elif Input.is_action_just_released("left"):
-		direction_keys.erase("left")
-	if Input.is_action_just_pressed("down"):
-		direction_keys.push_back("down")
-	elif Input.is_action_just_released("down"):
-		direction_keys.erase("down")
-	if Input.is_action_just_pressed("up"):
-		direction_keys.push_back("up")
-	elif Input.is_action_just_released("up"):
-		direction_keys.erase("up")
-	if !Input.is_action_pressed("right") and !Input.is_action_pressed("left") and !Input.is_action_pressed("down") and !Input.is_action_pressed("up"):
-		direction_keys.clear()
+#	if Input.is_action_just_pressed("right"):
+#		direction_keys.push_back("right")
+#	elif Input.is_action_just_released("right"):
+#		direction_keys.erase("right")
+#	if Input.is_action_just_pressed("left"):
+#		direction_keys.push_back("left")
+#	elif Input.is_action_just_released("left"):
+#		direction_keys.erase("left")
+#	if Input.is_action_just_pressed("down"):
+#		direction_keys.push_back("down")
+#	elif Input.is_action_just_released("down"):
+#		direction_keys.erase("down")
+#	if Input.is_action_just_pressed("up"):
+#		direction_keys.push_back("up")
+#	elif Input.is_action_just_released("up"):
+#		direction_keys.erase("up")
+#	if !Input.is_action_pressed("right") and !Input.is_action_pressed("left") and !Input.is_action_pressed("down") and !Input.is_action_pressed("up"):
+#		direction_keys.clear()
 
 func movement_input2():
 	var directions = ["right", "left", "down", "up"]
